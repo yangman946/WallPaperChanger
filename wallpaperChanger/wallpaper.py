6b77ab19fa704983a29271349711961d7eb75541
Mensch272 <47662901+mensch272@users.noreply.github.com>
2021-09-08 15:35:24 +0500

    fix file exists check

--- wallpaperChanger/wallpaper.py
+++ wallpaperChanger/wallpaper.py
@@ -1,6 +1,8 @@
 import platform
 import subprocess
+from functools import wraps
 from pathlib import Path
+from typing import Callable
 
 from wallpaperChanger.exceptions import PlatformNotSupportedException
 
@@ -10,23 +12,35 @@ system = platform.system()  # https://docs.python.org/3/library/platform.html#pl
 # alternative to calling a method and catching the exception
 system_supported = True
 
+
+def _ensure_exists(func: Callable[[Path], None]):
+    """Decorator that ensures that the file in the argument exists"""
+
+    @wraps(func)
+    def wrapper(file: Path):
+        if not file.exists() or not file.is_file():
+            raise FileNotFoundError(f"'{file}' was not found.")
+
+        return func(file)
+
+    return wrapper
+
+
 if system == 'Windows':
     import ctypes
 
-
+    @_ensure_exists
     def set_wallpaper(file: Path):
         """Change windows wallpaper to the provided file
 
         :raises FileNotFoundError: if the provided file does not exist
         """
-        if not file.exists() or not file.is_file():
-            raise FileNotFoundError(f"'{file}' was not found.")
-
         ctypes.windll.user32.SystemParametersInfoW(20, 0, str(file), 0)
 
 elif system == 'Linux':
 
     # TODO add support for kde desktops
+    @_ensure_exists
     def set_wallpaper(file: Path):
         """Change linux wallpaper to the provided file
 
