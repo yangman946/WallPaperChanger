882767244d4e8e8728c3d523a2a1ae8df9fadb92
Mensch272 <47662901+mensch272@users.noreply.github.com>
2021-09-08 15:15:23 +0500

    added support for linux gnome desktops

--- wallpaperChanger/wallpaper.py
+++ wallpaperChanger/wallpaper.py
@@ -1,9 +1,11 @@
 import platform
+import subprocess
 from pathlib import Path
 
 from wallpaperChanger.exceptions import PlatformNotSupportedException
 
-system = platform.system()  # https://docs.python.org/3/library/platform.html#platform.system
+# system = platform.system()  # https://docs.python.org/3/library/platform.html#platform.system
+system = ''
 
 # Used to identify is a system has support to change wallpaper
 # alternative to calling a method and catching the exception
@@ -24,12 +26,21 @@ if system == 'Windows':
         ctypes.windll.user32.SystemParametersInfoW(20, 0, str(file), 0)
 
 elif system == 'Linux':
-    pass
+    # TODO add support for kde desktops
+    def set_wallpaper(file: Path):
+        """Change linux wallpaper to the provided file
+
+        :raises FileNotFoundError: if the provided file does not exist
+        """
+        subprocess.Popen(
+            f"gsettings set org.gnome.desktop.background picture-uri 'file://{file}'",
+            shell=True,
+        )
 
 else:
     system_supported = False
 
 
     def set_wallpaper(file: Path):
-        """Dummy method that throws a PlatformNotSupportedException when called"""
+        """Placeholder function that throws a PlatformNotSupportedException when called"""
         raise PlatformNotSupportedException(f"'{system}' does not currently have support to change wallpaper.")
