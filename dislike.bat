a3f580cd8c6d2c4578faf3e3bb738d7c88d9d589
Clarence Yang <clarenceyang284@gmail.com>
2023-04-24 17:38:28 +1000

    update final

--- dislike.bat
+++ dislike.bat
@@ -1,9 +1,9 @@
 @echo off
 
 @REM add the current wallpaper to blacklisted wallpapers 
-type C:\Users\Clarence\Documents\WallPaperChanger-main\debug\current_wallpaper.txt >> C:\Users\Clarence\Documents\WallPaperChanger-main\debug\blacklisted_wallpapers.txt
+type C:\Users\Clarence\Documents\GitHub\WallPaperChanger\debug\current_wallpaper.txt >> C:\Users\Clarence\Documents\GitHub\WallPaperChanger\debug\blacklisted_wallpapers.txt
 @REM refresh wallpapers
-cd C:\Users\Clarence\Documents\WallPaperChanger-main
+cd C:\Users\Clarence\Documents\GitHub\WallPaperChanger
 
 @REM pythonw or python-window-mode doesnt show the console
 start /MIN pythonw -m wallpaperChanger none
